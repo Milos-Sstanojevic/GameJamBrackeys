de966e3704343cef75e2a1ad1bf77b93b67ebe9d
Aleksandar <prokopovic75@gmail.com>
2024-02-15 21:42:29 +0100

    Moving platform 2.0

--- Assets/Scripts/MovingPlatform/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform/MovingPlatform.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -7,20 +8,54 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] float timeOffset;
     [SerializeField] float movingSpeed;
-    [SerializeField] Vector3 range;
+    [SerializeField] Vector3 target1;
+    [SerializeField] Vector3 target2;
+    [SerializeField] float minDiff;
+    [SerializeField] float getAwayDistance;
 
-    private Vector3 homePosition;
+    private int currentTarget;
+
+    private Collider2D myCollider;
+    private ContactFilter2D filter;
+    private List<Collider2D> result;
     void Start()
     {
-        homePosition = transform.position;
+        currentTarget = 1;
+        myCollider = GetComponent<Collider2D>();
+        filter = new ContactFilter2D().NoFilter();
+        result = new();
     }
     void Update()
     {
-        float t = Mathf.Sin(timeOffset + Time.time * movingSpeed) + 1f / 2f;
-        transform.position = homePosition + t * range;
+        var targetPosition = currentTarget == 1 ? target1 : target2;
+        var diff = targetPosition - transform.position;
+
+        transform.Translate(diff.normalized * movingSpeed * Time.deltaTime);
+
+        if ((diff.magnitude < minDiff))
+        {
+            SwitchDirection();
+        }
+        else
+        {
+            myCollider.OverlapCollider(filter, result);
+            if (result.Any(collider => collider.CompareTag("Platform")))
+            {
+                SwitchDirection();
+            }
+            result.Clear();
+        }
+    }
+    private void SwitchDirection()
+    {
+        currentTarget = currentTarget == 1 ? 2 : 1;
+        var targetPosition = currentTarget == 1 ? target1 : target2;
+        var diff = targetPosition - transform.position;
+        transform.Translate(diff.normalized * getAwayDistance);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Collision");
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.SetParent(transform);
