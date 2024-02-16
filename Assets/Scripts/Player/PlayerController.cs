fca70a667de139d88c7bff419ebb79e774b6736f
Aleksandar <prokopovic75@gmail.com>
2024-02-16 14:36:42 +0100

    Wall jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -19,10 +19,10 @@ public class PlayerController : MonoBehaviour
 
     private int leftJumps;
     private bool jumpPressed;
-    private float lastTimeGrounded;
+    private float lastTimeJumpsReseted;
     private float lastTimeJumpPressed;
 
-    private GameObject lastTouchedWall;
+    private GameObject lastWallUsedForReset;
 
 
     private Rigidbody2D rb;
@@ -34,14 +34,14 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         collisionComponent = GetComponent<PlayerCollision>();
 
-        lastTouchedWall = null;
+        lastWallUsedForReset = null;
     }
 
     void Start()
     {
         leftJumps = MaxJumps;
         jumpPressed = false;
-        lastTimeGrounded = float.MinValue;
+        lastTimeJumpsReseted = float.MinValue;
         lastTimeJumpPressed = float.MinValue;
 
     }
@@ -57,104 +57,61 @@ public class PlayerController : MonoBehaviour
         var isGrounded = collisionComponent.IsGrounded;
         var isTouchingWall = collisionComponent.IsTouchingWall;
 
-        //}
-        //private void FixedUpdate()
-        //{
 
-        var shouldGetWallReset = false;
-        if (isTouchingWall)
-        {
-            var currentlyTouchedWall = collisionComponent.LastTouchedWall;
-            var isTouchingDifferentWall = currentlyTouchedWall != lastTouchedWall;
-
-            var couldGetWallReset = isTouchingWall && isTouchingDifferentWall;
-            shouldGetWallReset = couldGetWallReset && WallJumpEnabled;
-
-            lastTouchedWall = currentlyTouchedWall;
-        }
-        if (shouldGetWallReset)
-            Debug.Log(shouldGetWallReset);
-
-
-        bool coyoteTimeFulfilled = !isGrounded && (Time.time - lastTimeGrounded) < CoyoteTime;
+        bool coyoteTimeFulfilled = (Time.time - lastTimeJumpsReseted) < CoyoteTime;
         bool jumpBufferTimeFulfilled = (Time.time - lastTimeJumpPressed) < JumpBufferTime;
 
-        //lastTimeGrounded = isGrounded ? Time.time : lastTimeGrounded;
-
-        // TODO: izbegni magiju
-        /* Ovo radi magijom
-         * 
-         * shouldGetWallReset je true samo jedan jedini frame
-         * i to prvi put kad se detektuje da dodirujes zid
-         * Taj frejm ti dobijes maxJumps u leftJumps
-         * ali (da nije coyot time-a) oni bi ti se u sledeci frejm odma smanjili jer vise nije true
-         * tako da max skokoke bi dobio samo ako bas u taj frejm skoknes
-         * medjutim coyot time me ovde spasava 
-         * sve dok on ne istekne ne smanjuju mi se leftJumps
-         * 
-         */
-        if (isGrounded || shouldGetWallReset)
+        if (isGrounded)
         {
-            leftJumps = MaxJumps;
-            lastTimeGrounded = Time.time;
-            if (isGrounded)
+            lastTimeJumpsReseted = Time.time;
+            lastWallUsedForReset = null;
+        }
+
+        if (isTouchingWall)
+        {
+            if (lastWallUsedForReset != collisionComponent.LastTouchedWall)
             {
-                lastTouchedWall = null;
+                lastTimeJumpsReseted = Time.time;
+                lastWallUsedForReset = collisionComponent.LastTouchedWall;
             }
         }
-        else if (leftJumps == MaxJumps)
+
+        if (coyoteTimeFulfilled)
         {
-            if (!coyoteTimeFulfilled)
+            leftJumps = MaxJumps;
+        }
+        else
+        {
+            if (leftJumps == MaxJumps)
             {
                 leftJumps = MaxJumps - 1;
             }
         }
 
-        if (leftJumps > 0 && jumpPressed)
+        if (jumpPressed)
         {
-            rb.velocity = Vector3.zero;
-            rb.AddForce(JumpStrength * Vector3.up, ForceMode2D.Impulse);
-            leftJumps--;
-            jumpPressed = false;
+            if(leftJumps > 0)
+            {
+                if (leftJumps == MaxJumps)
+                {
+                    lastTimeJumpsReseted = 0;
+                }
+                leftJumps = leftJumps - 1;
+                Jump();
+                jumpPressed = false;
+            }
         }
-        else if (!jumpBufferTimeFulfilled)
+
+        if (!jumpBufferTimeFulfilled)
         {
             jumpPressed = false;
         }
 
         transform.Translate(MovementSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal") * Vector3.right);
     }
-
-    //private void PlaceDoor()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.G))
-    //    {
-    //        DoorsController door = PlayerDoorInventory.Instance.GetGreenDoors();
-    //        if (door != null)
-    //        {
-    //            door.gameObject.SetActive(true);
-    //            EventManager.Instance.OnPlaceDoorHorizontally(door, new Vector3(transform.position.x + 2, transform.position.y, transform.position.z));
-    //        }
-    //    }
-
-    //    if (Input.GetKeyDown(KeyCode.R))
-    //    {
-    //        DoorsController door = PlayerDoorInventory.Instance.GetRedDoors();
-    //        if (door != null)
-    //        {
-    //            door.gameObject.SetActive(true);
-    //            EventManager.Instance.OnPlaceDoorHorizontally(door, new Vector3(transform.position.x + 2, transform.position.y, transform.position.z));
-    //        }
-    //    }
-
-    //    if (Input.GetKeyDown(KeyCode.B))
-    //    {
-    //        DoorsController door = PlayerDoorInventory.Instance.GetBlueDoors();
-    //        if (door != null)
-    //        {
-    //            door.gameObject.SetActive(true);
-    //            EventManager.Instance.OnPlaceDoorHorizontally(door, new Vector3(transform.position.x + 2, transform.position.y, transform.position.z));
-    //        }
-    //    }
-    //}
+    private void Jump()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(JumpStrength * Vector3.up, ForceMode2D.Impulse);
+    }
 }
